30b6db51ba16bb4f137d5bf0bb9a7a7cea3f9fe3
zhaoxue1688 <zhaoxue1688@126.com>
2015-08-27 16:58:35 +0800

    提交测试js

---
+++ js/abc2.js
@@ -0,0 +1,24 @@
+/*魏如博众筹页*/
+function the_raise(){
+		var _this = this;
+		//默认调用所有方法
+		_this.the_raise_list = function(){/*接口调用*/
+			$.ajax({
+			type: "get",
+			//data: params,
+			url: "js/activeNoticeEditor.json",
+			//url: url_host + "/admin/findActivityNoticeOne.action",
+			dataType: "json",
+				success: function(data) {
+					console.log(data);
+				}
+			});
+		};
+		_this.the_raise_list();
+}
+
+$(function(){
+	if ($('body').length > 0) {
+        the_raise();
+    }
+});
