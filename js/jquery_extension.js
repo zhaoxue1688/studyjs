39be16f995f1430ec8cb2de8126e3fce61ac7e17
zhaoxue1688 <zhaoxue1688@126.com>
2015-08-28 17:39:26 +0800

    增加jquery扩展插件,可设置字体颜色，背景色，字体大小

--- js/jquery_extension.js
+++ js/jquery_extension.js
@@ -1,14 +1,26 @@
-$.fn.hightlight1 = function () {
-	this.css('backgroundColor','#fffceb').css('color','green');
-	return this;
-};
-
-$.fn.hightlight2 = function (options) {
-    // 要考虑到各种情况:
-    // options为undefined
-    // options只有部分key
-    var bgcolor = options && options.backgroundColor || '#fffceb';
-    var color = options && options.color || '#d85030';
-    this.css('backgroundColor', bgcolor).css('color', color);
+$.fn.highlight = function (options) {
+    // 合并默认值和用户设定值:
+    var opts = $.extend({}, $.fn.highlight.defaults, options);
+    this.css('background-Color', opts.backgroundColor).css('color', opts.color);
     return this;
 };
+
+// 设定背景色，字体颜色默认值:
+$.fn.highlight.defaults = {
+    color: '#d85030',
+    backgroundColor: '#fff8de'
+}
+
+//==========================制作字体大小插件
+
+$.fn.fontsize = function (options) {
+    // 合并默认值和用户设定值:
+    var opts = $.extend({}, $.fn.fontsize.defaults, options);
+    this.css('font-size', opts.fontsize);
+    return this;
+};
+
+// 设定字体大小默认值:
+$.fn.fontsize.defaults = {
+     fontsize: '20px'
+}
